cb415c7bd7edaeff6dbb77be385347ebbf460fa7
Nurbek Asanbekov <nasanbekov@gmail.com>
2021-08-11 13:28:53 +0300

    added readme

---
+++ README.md
@@ -0,0 +1,18 @@
+## String method call
+
+This code snippet shows how to call functions(methods) having just it's string name.
+
+No need "extern "C"" for the callee. Name demangling handled correctly.
+
+
+### Requirements
+
+- cmake
+- g++
+
+### Build
+
+```sh
+mkdir build && cd build && cmake .. && make
+```
+
--- main.cpp
+++ main.cpp
@@ -20,11 +20,10 @@ public:
     int init(std::string &library_name)
     {
         void *library_handle;
-
         library_handle = dlopen(library_name.c_str(), RTLD_LAZY);
 
         if (!library_handle) {
-            std::cerr << "Error: " << dlerror() << std::endl; 
+            std::cerr << "Error: " << dlerror() << std::endl;
             return -1;
         }
 
@@ -57,23 +56,19 @@ public:
                 char *demangled_name = abi::__cxa_demangle(str, NULL, NULL, &status);
                 if (demangled_name) {
                     std::string strDemangledName(demangled_name);
-                    
                     std::cout << strDemangledName << std::endl;
-                    
-                    //if (strDemangledName == "CMyClass::CMyClass()")
-                    
-                    if (strDemangledName == "get_instance()")
-                    {
+                    //if (strDemangledName == "CMyClass::CMyClass()") <- for some reason direct call to ctor doesn't work
+                    if (strDemangledName == "get_instance()") {
                         func_cmyclass_ctor_t ctor = (func_cmyclass_ctor_t)dlsym(library_handle, str);
                         m_pStatechartObject = std::make_unique<CMyClass>(*ctor());
-
                         continue;
                     }
                     if (strDemangledName.rfind("CMyClass::raiseEvent") == 0) {
-
                         func_raiseEvent_t func_raiseEvent = (func_raiseEvent_t)dlsym(library_handle, str);
-                        if (!func_raiseEvent)
+                        if (!func_raiseEvent) {
+                            std::free(demangled_name);
                             continue;
+                        }
                         m_mFunctionsTable[strDemangledName] = std::make_unique<func_raiseEvent_t>(func_raiseEvent);
                     }
                     std::free(demangled_name);
@@ -86,8 +81,7 @@ public:
     bool exec_procedure(const std::string &procedure_name)
     {
         auto func = m_mFunctionsTable.find(procedure_name);
-        if (func != m_mFunctionsTable.end())
-        {
+        if (func != m_mFunctionsTable.end()) {
             (*func->second)(m_pStatechartObject.get());
             return true;
         }
@@ -111,97 +105,5 @@ int main()
     e.exec_procedure("CMyClass::raiseEvent3()");
     e.exec_procedure("CMyClass::raiseEvent1()");
 
-
-    /*void *handle;
-
-    
-
-    void (*func_raiseEvent)(CMyClass *);
-
-    CMyClass *p_my_object = nullptr;
-
-    handle = dlopen("./libshared.so", RTLD_LAZY);
-
-    if (!handle)
-    {
-        std::cerr << "Error: " << dlerror() << std::endl; 
-        return EXIT_FAILURE;
-    }
-   
-    // list all symbols from .so file
-
-    struct link_map *map = nullptr;
-    dlinfo(handle, RTLD_DI_LINKMAP, &map);
-
-    Elf64_Sym *symtab = nullptr;
-    char *strtab = nullptr;
-    int symentries = 0;
-
-
-    for (auto section = map->l_ld; section->d_tag != DT_NULL; ++section) {
-        if (section->d_tag == DT_SYMTAB) {
-            symtab = (Elf64_Sym *)section->d_un.d_ptr;
-        }
-        if (section->d_tag == DT_STRTAB) {
-            strtab = (char*)section->d_un.d_ptr;
-        }
-        if (section->d_tag == DT_SYMENT) {
-            symentries = section->d_un.d_val;
-        }
-    }
-    int size = strtab - (char *)symtab;
-    for (int k = 0; k < size / symentries; ++k) {
-        auto sym = &symtab[k];
-        if (ELF64_ST_TYPE(symtab[k].st_info) == STT_FUNC) {
-            char *str = &strtab[sym->st_name];
-
-            // {
-            int status = -1;
-            char *demangled_name = abi::__cxa_demangle(str, NULL, NULL, &status);
-            if (demangled_name) {
-                std::string strDemangledName(demangled_name);
-                std::cout << strDemangledName << std::endl;
-                if (strDemangledName.rfind("CMyClass::raiseEvent") == 0) {
-                    *(void**)(&func_raiseEvent) = dlsym(handle, str);
-                    if (!func_raiseEvent) {
-                        std::cerr << "Error: " << dlerror() << std::endl;
-                        dlclose(handle);
-                        return EXIT_FAILURE;
-                    }
-                    func_raiseEvent(p_my_object);
-                }
-
-
-            }
-            std::free(demangled_name);
-            // {
-            //printf("%s\n", str);
-        }
-    }
-    // end of printing symbol list
-
-
-
-
-    *(void**)(&func_raiseEvent) = dlsym(handle, "raiseEvent1");
-    if (!func_raiseEvent) {
-        std::cerr << "Error: " << dlerror() << std::endl;
-        dlclose(handle);
-        return EXIT_FAILURE;
-    }
-
-
-    func_raiseEvent(p_my_object);
-
-
-
-    dlclose(handle);
-*/
     return EXIT_SUCCESS;
-
-
-    //CMyClass *p_my_object = new CMyClass();
-    //p_my_object->raiseEvent1();
-    //delete p_my_object;
-
 }
